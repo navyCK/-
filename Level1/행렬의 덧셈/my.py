bb5c754f69255dd994168d34c7c6ae5ef0c2755f
navyCK <ckck81026@gmail.com>
2021-05-14 23:11:13 +0900

    210514

---
+++ Level1/행렬의 덧셈/my.py
@@ -0,0 +1,12 @@
+def solution(arr1, arr2):
+    tmp = []
+    answer = []
+    for i in range(len(arr1)) :
+        for j in range(len(arr1[i])) :
+            tmp.append(arr1[i][j] + arr2[i][j])
+        answer.append(tmp)
+        tmp = []
+    return answer
+
+print(solution([[1,2],[2,3]], [[3,4],[5,6]]))
+print(solution([[1],[2]], [[3],[4]]))
