f07cabd255309f1924258c812604e84a45ddca6d
navyCK <ckck81026@gmail.com>
2021-10-09 16:59:53 +0900

    211009

---
+++ BAEKJOON/단계별/기본 수학 1/달팽이/ceil.py
@@ -0,0 +1,5 @@
+import math
+
+A, B, V = map(int, input().split())
+day = (V - B) / (A - B)
+print(math.ceil(day))
---
+++ BAEKJOON/단계별/기본 수학 1/호텔/for.py
@@ -0,0 +1,9 @@
+T = int(input())
+for _ in range(T):
+    H, W, N = map(int, input().split())
+    floor = N % H
+    num = N // H + 1
+    if N % H == 0:
+        floor = H
+        num = N // H
+    print(floor*100 + num)
